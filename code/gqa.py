6425ce70f0433e80d20100f2ef99437ffb283dc4
Zhuohao-Li <garrick0508@gmail.com>
2025-11-11 14:12:15 -0800

    gqa update

--- code/gqa.py
+++ code/gqa.py
@@ -17,6 +17,7 @@ class GQA(nn.Module):
         self.wo = nn.Linear(num_heads * self.head_dim, dim, bias=False)
 
     def repeat_kv(self, x):
+        assert self.num_heads % self.n_kv_heads == 0
         n_rep = self.num_heads // self.n_kv_heads
         if n_rep == 1:
             return x
@@ -25,27 +26,28 @@ class GQA(nn.Module):
 
     def forward(self, x):
         bsz, seqlen, _ = x.shape
+        # x: (bsz, seqlen, dim)
 
-        q = self.wq(x)
-        k = self.wk(x)
-        v = self.wv(x)
+        q = self.wq(x) # (bsz, seqlen, num_heads * head_dim)
+        k = self.wk(x) # (bsz, seqlen, n_kv_heads * head_dim)
+        v = self.wv(x) # (bsz, seqlen, n_kv_heads * head_dim)
 
-        q = q.view(bsz, seqlen, self.num_heads, self.head_dim)
-        k = k.view(bsz, seqlen, self.n_kv_heads, self.head_dim)
-        v = v.view(bsz, seqlen, self.n_kv_heads, self.head_dim)
+        q = q.view(bsz, seqlen, self.num_heads, self.head_dim) # (bsz, seqlen, num_heads, head_dim)
+        k = k.view(bsz, seqlen, self.n_kv_heads, self.head_dim) # (bsz, seqlen, n_kv_heads, head_dim)
+        v = v.view(bsz, seqlen, self.n_kv_heads, self.head_dim) # (bsz, seqlen, n_kv_heads, head_dim)
 
-        k = self.repeat_kv(k)
-        v = self.repeat_kv(v)
+        k = self.repeat_kv(k) # (bsz, seqlen, num_heads, head_dim)
+        v = self.repeat_kv(v) # (bsz, seqlen, num_heads, head_dim)
 
-        q = q.transpose(1, 2)
-        k = k.permute(0, 2, 1, 3)
-        v = v.transpose(1, 2)
+        q = q.transpose(1, 2) # (bsz, num_heads, seqlen, head_dim)
+        k = k.transpose(1, 2) # (bsz, num_heads, seqlen, head_dim)
+        v = v.transpose(1, 2) # (bsz, num_heads, seqlen, head_dim)
 
-        scores = torch.matmul(q, k) / math.sqrt(self.head_dim)
-        attn = F.softmax(scores, dim=-1)
+        scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.head_dim) # (bsz, num_heads, seqlen, seqlen)
+        attn = F.softmax(scores, dim=-1) # (bsz, num_heads, seqlen, seqlen)
 
-        out = torch.matmul(attn, v)
-        out = out.transpose(1, 2)
-        out = out.reshape(bsz, seqlen, -1)
-        out = self.wo(out)
+        out = torch.matmul(attn, v) # (bsz, num_heads, seqlen, head_dim)
+        out = out.transpose(1, 2) # (bsz, seqlen, num_heads, head_dim)
+        out = out.reshape(bsz, seqlen, -1) # (bsz, seqlen, dim)
+        out = self.wo(out) # (bsz, seqlen, dim)
         return out
